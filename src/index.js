c7395cd62c6b300e7a573a690e95df86dd1e0886
Murad Rogozhnikov <murad@rogozhnikoff.com>
2015-03-06 21:58:28 +0530

    refresh by click && bit refactoring

--- src/index.js
+++ src/index.js
@@ -1,50 +1,46 @@
 /*
-* ideas: перевести шаг в проценты
-* */
+ * ideas: перевести шаг в проценты
+ * */
 
 var lastInternet,
     timer,
     options = {
         id: 'etisalat-internet',
+        url: 'https://mypage.etisalat.lk/bbportal/home',
         step: 100,
         timeoutSuccess: minToMl(3),
         timeoutFail: minToMl(10),
-        notificationTime: 3000
-    };
+        notificationTime: 3000,
+        forNotifications: {
+            type: 'basic',
+            message: 'checker-message',
+            priority: 1,
+            iconUrl: "icons/etisalat-logo.png"
+        }
+    }
 
 // first call
 refresh();
 
-chrome.browserAction.onClicked.addListener(function() {
+chrome.browserAction.onClicked.addListener(function () {
     clearTimeout(timer);
     refresh();
 });
 
-function minToMl(min) {
-    return min * (60 * 1000)
-}
-
-function isOffPeak() {
-    return (new Date()).getHours() < 8
-}
-function getSelectorByTime() {
-    return isOffPeak() ? 'remainingValOffPeak' : 'remainingValPeak'
-}
-
-function getInternet(){
+function getInternet() {
     var dfd = $.Deferred();
 
     $.ajax({
-        url: 'https://mypage.etisalat.lk/bbportal/home'
-    }).done(function(html){
+        url: options.url
+    }).done(function (html) {
         dfd.resolve({
-           quota: parseInt(
-               html.match(
-                   new RegExp('<label id="' + getSelectorByTime() + '">\\s+([0-9]+)\\s+[A-z]+\\s+</label>', 'im')
-               )[1]
-           )
+            quota: parseInt(
+                html.match(
+                    new RegExp('<label id="' + getSelectorByTime() + '">\\s+([0-9]+)\\s+[A-z]+\\s+</label>', 'im')
+                )[1]
+            )
         });
-    }).fail(function(status){
+    }).fail(function (status) {
         dfd.reject({
             status: status
         });
@@ -53,39 +49,24 @@ function getInternet(){
     return dfd.promise();
 }
 
-function notify(data){
-    chrome.notifications.create(options.id, {
-        type: 'basic',
-        title: 'Йо, интернета осталось ' + data.quota + ' mb.',
-        message: 'fuckMessage',
-        priority: 1,
-        eventTime: Date.now() + 1000,
-        iconUrl: "icons/etisalat-logo.png"
-    }, function(id){});
+function notify(data) {
+    chrome.notifications.create(
+        options.id,
+        extend(
+            options.forNotifications,
+            {title: 'Йо, интернета осталось ' + data.quota + ' mb.'}
+        ),
+        noop
+    );
 
-    setTimeout(function(){
-        chrome.notifications.clear(options.id, function(status){})
+    setTimeout(function () {
+        chrome.notifications.clear(options.id, noop)
     }, options.notificationTime);
 }
 
-function isString(some) {
-    return typeof some === 'string'
-}
 
-function transformToString(some) {
-    return some + '';
-}
-
-function sliceMoreFour(str) {
-    if (!isString(str)) str = transformToString(str);
-
-    if (str.length <= 4) return str;
-
-    return str.slice(0, 4)
-}
-
-function refresh(){
-    getInternet().done(function(data){
+function refresh() {
+    getInternet().done(function (data) {
         var firstCall = typeof lastInternet === 'undefined',
             step = (data.quota > 100) ? options.step : 25;
 
@@ -100,7 +81,54 @@ function refresh(){
 
         timer = setTimeout(refresh, options.timeoutSuccess);
 
-    }).fail(function(){
+    }).fail(function () {
         timer = setTimeout(refresh, options.timeoutFail);
     });
+}
+
+
+/*
+ * Clean helpers function
+ * */
+
+function noop() {
+
+}
+
+function extend(x, y) {
+    var ret = {};
+    for (var xname in x) {
+        if(x.hasOwnProperty(xname)) {
+            ret[xname] = typeof y[xname] !== 'undefined' ? y[xname] : x[xname];
+        }
+    }
+    for (var yname in y) {
+        if(y.hasOwnProperty(yname)) {
+            ret[yname] = y[yname]
+        }
+    }
+    return ret;
+}
+function minToMl(min) {
+    return min * (60 * 1000)
+}
+
+function isOffPeak() {
+    return (new Date()).getHours() < 8
+}
+function getSelectorByTime() {
+    return isOffPeak() ? 'remainingValOffPeak' : 'remainingValPeak'
+}
+function isString(some) {
+    return typeof some === 'string'
+}
+function transformToString(some) {
+    return some + '';
+}
+function sliceMoreFour(str) {
+    if (!isString(str)) str = transformToString(str);
+
+    if (str.length <= 4) return str;
+
+    return str.slice(0, 4)
 }
